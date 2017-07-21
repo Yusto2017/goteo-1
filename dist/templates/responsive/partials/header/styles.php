7dde04aab40750ae684b5ac7b419d72769a062c4
dessHub <deskip95@gmail.com>
2017-07-21 12:40:54 +0300

    g

---
+++ dist/templates/responsive/partials/header/styles.php
@@ -0,0 +1,11 @@
+<!-- Bootstrap core CSS -->
+<link href="<?= SRC_URL ?>/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+<link href="<?= SRC_URL ?>/assets/vendor/font-awesome/css/font-awesome.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css?family=Roboto:400,700" rel="stylesheet" type="text/css">
+<link href="https://fonts.googleapis.com/css?family=Open+Sans+Condensed:300" rel="stylesheet" type="text/css">
+<link href="<?= SRC_URL ?>/assets/vendor/footable/footable.min.css" rel="stylesheet">
+
+<!-- POST PROCESSING THIS CSS BY GRUNT -->
+
+<link href="<?= SRC_URL ?>/assets/css/all.1eab90c1.css" type="text/css" rel="stylesheet" />
+
